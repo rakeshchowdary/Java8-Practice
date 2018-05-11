b89ca0b41343bf6c1d4f57b0c699a7c3c5ebba3b
Rakesh <Rakesh@192.168.0.104>
2018-05-11 07:52:13 +0530

    Project Name updated

--- Java8Practice/src/org/java8/rakesh/Apple.java
+++ Java8Practice/src/org/java8/rakesh/Apple.java
@@ -1,11 +1,54 @@
 package org.java8.rakesh;
 
-public class Apple {
+import java.util.ArrayList;
+import java.util.List;
 
-	public String getColour(Apple apple) {
+interface Predicate<T> {
+	boolean test(T t);
+}
+
+public class Apple {
+	public List<Apple> inventory;
+
+	public String getColour() {
 
 		return null;
 
 	}
+
+	public int getWeight() {
+		return 0;
+	}
+
+	public static List<Apple> getGreenApples(List<Apple> inventory) {
+		List<Apple> greenAppleList = new ArrayList<>();
+		for (Apple a : inventory) {
+			if ("green".equals(a.getColour())) {
+				greenAppleList.add(a);
+			}
+		}
+		return greenAppleList;
+	}
+
+	public static boolean isGreenApple(Apple apple) {
+		return "green".equals(apple.getColour());
+	}
+
+	public boolean isHeavyApple(Apple apple) {
+		return apple.getWeight() > 150;
+	}
+
+	static List<Apple> filterApples(List<Apple> inventory, Predicate p) {
+		List<Apple> result = new ArrayList<>();
+		for (Apple a : inventory) {
+			if (p.test(a)) {
+				result.add(a);
+			}
+		}
+
+		return result;
+	}
+	
+	
 
 }
